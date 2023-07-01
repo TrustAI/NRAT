968a7fe6225da1f2e92ec81e9009389b1c4557cf
czhen97 <135268053+czhen97@users.noreply.github.com>
2023-07-01 13:27:31 +0100

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,166 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+from tqdm import tqdm
+from numpy.testing import assert_array_almost_equal
+import numpy as np
+import os
+import torch
+import random
+import mlconfig
+
+
+def multiclass_noisify(y, P, random_state=0):
+    """ Flip classes according to transition probability matrix T.
+    It expects a number between 0 and the number of classes - 1.
+    """
+
+    assert P.shape[0] == P.shape[1]
+    assert np.max(y) < P.shape[0]
+
+    # row stochastic matrix
+    assert_array_almost_equal(P.sum(axis=1), np.ones(P.shape[1]))
+    assert (P >= 0.0).all()
+
+    m = y.shape[0]
+    new_y = y.copy()
+    flipper = np.random.RandomState(random_state)
+
+    for idx in np.arange(m):
+        i = y[idx]
+        # draw a vector with only an 1
+        flipped = flipper.multinomial(1, P[i, :], 1)[0]
+        new_y[idx] = np.where(flipped == 1)[0]
+
+    return new_y
+
+
+def other_class(n_classes, current_class):
+    """
+    Returns a list of class indices excluding the class indexed by class_ind
+    :param nb_classes: number of classes in the task
+    :param class_ind: the class index to be omitted
+    :return: one random class that != class_ind
+    """
+    if current_class < 0 or current_class >= n_classes:
+        error_str = "class_ind must be within the range (0, nb_classes - 1)"
+        raise ValueError(error_str)
+
+    other_class_list = list(range(n_classes))
+    other_class_list.remove(current_class)
+    other_class = np.random.choice(other_class_list)
+    return other_class
+
+
+class cifar10Nosiy(datasets.CIFAR10):
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=True, nosiy_rate=0.0, asym=False):
+        super(cifar10Nosiy, self).__init__(root, transform=transform, target_transform=target_transform)
+        self.download = download
+        if asym:
+            # automobile < - truck, bird -> airplane, cat <-> dog, deer -> horse
+            source_class = [9, 2, 3, 5, 4]
+            target_class = [1, 0, 5, 3, 7]
+            for s, t in zip(source_class, target_class):
+                cls_idx = np.where(np.array(self.targets) == s)[0]
+                n_noisy = int(nosiy_rate * cls_idx.shape[0])
+                noisy_sample_index = np.random.choice(cls_idx, n_noisy, replace=False)
+                for idx in noisy_sample_index:
+                    self.targets[idx] = t
+            return
+        elif nosiy_rate > 0:
+            n_samples = len(self.targets)
+            n_noisy = int(nosiy_rate * n_samples)
+            print("%d Noisy samples" % (n_noisy))
+            class_index = [np.where(np.array(self.targets) == i)[0] for i in range(10)]
+            class_noisy = int(n_noisy / 10)
+            noisy_idx = []
+            for d in range(10):
+                noisy_class_index = np.random.choice(class_index[d], class_noisy, replace=False)
+                noisy_idx.extend(noisy_class_index)
+                print("Class %d, number of noisy % d" % (d, len(noisy_class_index)))
+            for i in noisy_idx:
+                self.targets[i] = other_class(n_classes=10, current_class=self.targets[i])
+            print(len(noisy_idx))
+            print("Print noisy label generation statistics:")
+            for i in range(10):
+                n_noisy = np.sum(np.array(self.targets) == i)
+                print("Noisy class %s, has %s samples." % (i, n_noisy))
+            return
+
+
+@mlconfig.register
+class DatasetGenerator():
+    def __init__(self,
+                 train_batch_size=128,
+                 eval_batch_size=256,
+                 data_path='cifar-data/cifar10/',
+                 seed=1,
+                 num_of_workers=4,
+                 asym=False,
+                 dataset_type='CIFAR10',
+                 cutout_length=16,
+                 noise_rate=0.4):
+        self.seed = seed
+        np.random.seed(seed)
+        self.train_batch_size = train_batch_size
+        self.eval_batch_size = eval_batch_size
+        self.data_path = data_path
+        self.num_of_workers = num_of_workers
+        self.cutout_length = cutout_length
+        self.noise_rate = noise_rate
+        self.dataset_type = dataset_type
+        self.asym = asym
+        self.data_loaders = self.loadData()
+        return
+
+    def getDataLoader(self):
+        return self.data_loaders
+
+    def loadData(self):
+        CIFAR_MEAN = [0.49139968, 0.48215827, 0.44653124]
+        CIFAR_STD = [0.24703233, 0.24348505, 0.26158768]
+
+        train_transform = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR_MEAN, CIFAR_STD)])   # normalize
+
+        test_transform = transforms.Compose([                   
+            transforms.ToTensor(),
+            transforms.Normalize(CIFAR_MEAN, CIFAR_STD)])
+
+        train_dataset = cifar10Nosiy(root=self.data_path,
+                                        train=True,
+                                        transform=train_transform,
+                                        download=True,
+                                        asym=self.asym,
+                                        nosiy_rate=self.noise_rate)
+
+        test_dataset = datasets.CIFAR10(root=self.data_path,
+                                        train=False,
+                                        transform=test_transform,
+                                        download=True)
+        #else:
+        #    raise("Unknown Dataset")
+
+        data_loaders = {}
+
+        data_loaders['train_dataset'] = DataLoader(dataset=train_dataset,
+                                                   batch_size=self.train_batch_size,
+                                                   shuffle=True,
+                                                   pin_memory=True,
+                                                   num_workers=self.num_of_workers)
+
+        data_loaders['test_dataset'] = DataLoader(dataset=test_dataset,
+                                                  batch_size=self.eval_batch_size,
+                                                  shuffle=False,
+                                                  pin_memory=True,
+                                                  num_workers=self.num_of_workers)
+
+        print("Num of train %d" % (len(train_dataset)))
+        print("Num of test %d" % (len(test_dataset)))
+
+        return data_loaders
+
+
