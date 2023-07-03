ea72a9c1e6cbda92de8885a01b85ee7553539f88
czhen97 <135268053+czhen97@users.noreply.github.com>
2023-07-03 17:04:58 +0100

    Update at.py

--- core/utils/at.py
+++ core/utils/at.py
@@ -26,14 +26,12 @@ def at_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031, pert
     batch_size = len(x_natural)
     # generate adversarial example
     x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
-    #p_natural = F.softmax(model(x_natural), dim=1)
+    
     
     if attack == 'linf-pgd':
         for _ in range(perturb_steps):
             x_adv.requires_grad_()
-            #with torch.enable_grad():
-            #    loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1), p_natural)
-            #grad = torch.autograd.grad(loss_kl, [x_adv])[0]
+            
             x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
             x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
             x_adv = torch.clamp(x_adv, 0.0, 1.0)
@@ -77,9 +75,7 @@ def at_loss(model, x_natural, y, optimizer, step_size=0.003, epsilon=0.031, pert
     logits_natural = model(x_natural)
     logits_adv = model(x_adv)
     loss = F.cross_entropy(logits_adv, y)
-    #loss_robust = (1.0 / batch_size) * criterion_kl(F.log_softmax(logits_adv, dim=1),
-                                            #        F.softmax(logits_natural, dim=1))
-    #loss = loss_natural + beta * loss_robust
+
     
     batch_metrics = {'loss': loss.item(), 'clean_acc': accuracy(y, logits_natural.detach()), 
                      'adversarial_acc': accuracy(y, logits_adv.detach())}
